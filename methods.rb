ac84a3d9a2169e0ac4c609fba6e0476a1fa46ce3
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-03-24 17:21:31 -0400

    commit tree 039b79d62e5e3d0aca6fcf83c9f1e9c9dc37fb32

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+print greet_programmer
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+print greet
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+print greet_with_default()
+
+def add(num1,num2)
+    num1 + num2
+end
+
+def halve(num)
+   if num.class != Integer
+    return nil
+   end
+    num / 2
+end
